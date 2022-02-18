b0df128fd290be7d9647c33a606ee1f69d46c651
Leporoni <leporoni@hotmail.com>
2022-02-18 15:40:34 -0300

    Created and config listener class

---
+++ src/main/java/com/leporonitech/kafkaexample/KafkaListeners.java
@@ -0,0 +1,16 @@
+package com.leporonitech.kafkaexample;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+    @KafkaListener(
+            topics = "leporonitech",
+            groupId = "groupId"
+    )
+    void listener(String data) {
+        System.out.println("Listener received: " + data);
+    }
+}
